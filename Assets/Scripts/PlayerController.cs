ed058c0bc631af16486c30423b5b102fb25ac38d
Joey Weidman <pcjoe3343@gmail.com>
2018-05-28 01:10:31 -0600

    Player Movement
    Simple Player movement (non-physics based)

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private int speed; //Player movement speed
+
+	void Update()
+    {
+        Move();
+	}
+
+    /* For if we want to add boosts or slows */
+    public void ChangeSpeed(int speedChange)
+    {
+        //TODO: Adjust the speed variable
+    }
+    
+    private void Move()
+    {
+        if(Input.GetKey("w"))
+        {
+            transform.Translate(0, speed * Time.deltaTime, 0);
+        }
+        if (Input.GetKey("s"))
+        {
+            transform.Translate(0, -speed * Time.deltaTime, 0);
+        }
+        if (Input.GetKey("a"))
+        {
+            transform.Translate(-speed * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("d"))
+        {
+            transform.Translate(speed * Time.deltaTime, 0, 0);
+        }
+    }
+
+}
