ba46d2f115bad344abd2753c98f317dd5f1edd78
AvantAveGarde <coltonlee4407@hotmail.com>
2018-06-03 20:52:45 -0600

    Waypoint system for the kill objects

---
+++ Assets/Scripts/WaypointSystem.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointSystem : MonoBehaviour {
+
+    public bool cyclic;
+
+    public Vector3[] localWaypoints;
+
+    Vector3[] globalWaypoints;
+
+    public float speed;
+
+    int fromWaypointIndex;
+    float percentBetweenWaypoints;
+
+
+	// Use this for initialization
+	void Start () {
+
+        globalWaypoints = new Vector3[localWaypoints.Length];
+
+        for(int i = 0; i < globalWaypoints.Length; i++)
+        {
+            globalWaypoints[i] = localWaypoints[i] + transform.position;
+        }
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2 velocity = CalculateObjectMovement();
+
+        transform.Translate(velocity);
+
+	}
+
+    Vector3 CalculateObjectMovement()
+    {
+        fromWaypointIndex %= globalWaypoints.Length;
+
+        int toWaypointIndex = (fromWaypointIndex + 1) % globalWaypoints.Length;
+
+        float distanceBetweenWaypoints = Vector3.Distance(globalWaypoints[fromWaypointIndex], globalWaypoints[toWaypointIndex]);
+
+        percentBetweenWaypoints += Time.deltaTime * speed / distanceBetweenWaypoints;
+
+        Vector3 newPos = Vector3.Lerp(globalWaypoints[fromWaypointIndex], globalWaypoints[toWaypointIndex], percentBetweenWaypoints);
+
+        if(percentBetweenWaypoints >= 1)
+        {
+            percentBetweenWaypoints = 0;
+            fromWaypointIndex++;
+
+            if (!cyclic)
+                {
+                if (fromWaypointIndex >= globalWaypoints.Length - 1)
+                {
+                    fromWaypointIndex = 0;
+                    System.Array.Reverse(globalWaypoints);
+                }
+                else
+                {
+
+                }
+            }
+        }
+
+        return newPos - transform.position;
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (localWaypoints != null)
+        {
+            Gizmos.color = Color.red;
+            float size = .3f;
+
+            for (int i = 0; i < localWaypoints.Length; i++)
+            {
+                Vector3 globalWaypointPosition = (Application.isPlaying)?globalWaypoints[i] : localWaypoints[i] + transform.position;
+
+                Gizmos.DrawLine(globalWaypointPosition - Vector3.up * size, globalWaypointPosition + Vector3.up * size);
+                Gizmos.DrawLine(globalWaypointPosition - Vector3.left * size, globalWaypointPosition + Vector3.left * size);
+            }
+        }
+    }
+}
